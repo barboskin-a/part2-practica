9a29b8a7b1042559302be6448194e1161fc10051
Широких Елизавета Романовна <gr431_shiero@ttit.local>
2024-11-28 11:59:01 +0700

    8c

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category
+from .models import Request
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name') # отображение строк id и name
+    search_fields = ('name',) #поиск по name
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'category', 'status', 'created_at') # отображение строк id, title, user и тд.
+    list_filter = ('status', 'category')  #фильтрация по статусу и категории
+    search_fields = ('title', 'description')  #поиск по названию и описанию

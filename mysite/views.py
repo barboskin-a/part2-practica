a720cc873b82215bef4ee4d3d03ccaada6c401c6
Широких Елизавета Романовна <gr431_shiero@ttit.local>
2024-11-29 09:40:47 +0700

    изменение статуса

--- mysite/views.py
+++ mysite/views.py
@@ -26,6 +26,14 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
 
+#отображение информации относительно роли
+@login_required
+def profile(request):
+    user_role = 'Администратор' if request.user.is_staff else 'Пользователь' #проверяет роль и в зависимости от нее выполняет условия
+    return render(request, 'profile.html', {
+        'user_role': user_role
+    })
+
 #принимает заявки
 @login_required
 def my_requests(request):
@@ -73,3 +81,28 @@ def delete_category(request, pk):
         category.delete() #удаление
         return redirect('manage_categories') #возвращает на стр. управ категориями
     return render(request, 'delete_category.html', {'category': category}) #отправляет данный
+
+#изменение статуса заявки
+@staff_member_required
+def change_request_status(request, pk): #получает объект request, исп. перв-ый ключ pk
+    request_obj = get_object_or_404(Request, pk=pk)
+    if request.method == 'POST': #проверка метода post через условие
+        new_status = request.POST.get('status') #получает новый статус
+        if new_status == 'in_progress': #проверка статусов, сохранение и "вывод"
+            request_obj.status = 'in_progress'
+            request_obj.save()
+            return redirect('manage_requests')
+        elif new_status == 'completed':
+            if 'completed_image' in request.FILES: #проверяет наличие картинки
+                request_obj.status = 'completed'  #смена статуса и сохранение картинки
+                request_obj.image = request.FILES['completed_image']
+                request_obj.save()
+                return redirect('manage_requests')
+            else:
+                return render(request, 'change_status.html', {
+                    'request_obj': request_obj,
+                    'error': 'Для статуса «Выполнено» необходимо прикрепить изображение.'
+                })
+    return render(request, 'change_status.html', {
+        'request_obj': request_obj
+    })
